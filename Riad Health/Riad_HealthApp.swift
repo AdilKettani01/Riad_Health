7f921233038e4dc8d9abdbabcaed6c7244e44b5a
Adil Kettani <a.kettani01@gmail.com>
2026-08-29 21:43:51 +0100

    Initial Commit

---
+++ Riad Health/Riad_HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Riad_HealthApp.swift
+//  Riad Health
+//
+//  Created by Adil Kettani on 29/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct Riad_HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
